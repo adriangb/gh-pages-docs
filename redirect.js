dd22d65c2ea9ea94ded06e3267d0c30b1e306402
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-02-16 23:52:06 -0600

    assign

--- redirect.js
+++ redirect.js
@@ -16,11 +16,7 @@ $.getJSON(base_url + "versions.json", function (versions) {
     }
 });
 
-function maybeRedirect() {
-    if (((target_version !== null) && !(rest.startsWith("refs")))) {
-        // redirect
-        window.location.href = base_url.concat(target_version, "/", rest);
-    };
-    return false;
-}
-setTimeout(maybeRedirect, 1000);
+if (((target_version !== null) && !(rest.startsWith("refs")))) {
+    // redirect
+    document.location.assign(base_url.concat(target_version, "/", rest));
+};
